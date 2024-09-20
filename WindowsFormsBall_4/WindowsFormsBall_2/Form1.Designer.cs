26501781fa59ee99f5fc6ecf2659895586cd0b5e
Michael Thomas <129625504+Mitho33@users.noreply.github.com>
2024-09-20 12:44:30 +0200

    Add files via upload

---
+++ WindowsFormsBall_4/WindowsFormsBall_2/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WindowsFormsBall_2
+{
+    partial class FormBall
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.Panel2 = new System.Windows.Forms.Panel();
+            this.Panel10 = new System.Windows.Forms.Panel();
+            this.Panel4 = new System.Windows.Forms.Panel();
+            this.Panel11 = new System.Windows.Forms.Panel();
+            this.Panel9 = new System.Windows.Forms.Panel();
+            this.Panel12 = new System.Windows.Forms.Panel();
+            this.Ball = new System.Windows.Forms.Button();
+            this.Panel5 = new System.Windows.Forms.Panel();
+            this.Panel6 = new System.Windows.Forms.Panel();
+            this.Panel3 = new System.Windows.Forms.Panel();
+            this.Panel7 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel1
+            // 
+            this.Panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel1.Location = new System.Drawing.Point(261, 401);
+            this.Panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(71, 171);
+            this.Panel1.TabIndex = 0;
+            // 
+            // Panel2
+            // 
+            this.Panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel2.Location = new System.Drawing.Point(341, 86);
+            this.Panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(71, 249);
+            this.Panel2.TabIndex = 1;
+            // 
+            // Panel10
+            // 
+            this.Panel10.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel10.Location = new System.Drawing.Point(41, 668);
+            this.Panel10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel10.Name = "Panel10";
+            this.Panel10.Size = new System.Drawing.Size(1719, 43);
+            this.Panel10.TabIndex = 2;
+            // 
+            // Panel4
+            // 
+            this.Panel4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel4.Location = new System.Drawing.Point(715, 86);
+            this.Panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel4.Name = "Panel4";
+            this.Panel4.Size = new System.Drawing.Size(71, 302);
+            this.Panel4.TabIndex = 5;
+            // 
+            // Panel11
+            // 
+            this.Panel11.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel11.Location = new System.Drawing.Point(1753, 15);
+            this.Panel11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel11.Name = "Panel11";
+            this.Panel11.Size = new System.Drawing.Size(59, 697);
+            this.Panel11.TabIndex = 6;
+            // 
+            // Panel9
+            // 
+            this.Panel9.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel9.Location = new System.Drawing.Point(41, 15);
+            this.Panel9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel9.Name = "Panel9";
+            this.Panel9.Size = new System.Drawing.Size(1719, 43);
+            this.Panel9.TabIndex = 4;
+            // 
+            // Panel12
+            // 
+            this.Panel12.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel12.Location = new System.Drawing.Point(41, 54);
+            this.Panel12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel12.Name = "Panel12";
+            this.Panel12.Size = new System.Drawing.Size(55, 657);
+            this.Panel12.TabIndex = 7;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Ball.Location = new System.Drawing.Point(104, 65);
+            this.Ball.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(53, 50);
+            this.Ball.TabIndex = 8;
+            this.Ball.UseVisualStyleBackColor = false;
+            this.Ball.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BallKey);
+            // 
+            // Panel5
+            // 
+            this.Panel5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel5.Location = new System.Drawing.Point(924, 230);
+            this.Panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel5.Name = "Panel5";
+            this.Panel5.Size = new System.Drawing.Size(71, 302);
+            this.Panel5.TabIndex = 10;
+            // 
+            // Panel6
+            // 
+            this.Panel6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel6.Location = new System.Drawing.Point(1183, 100);
+            this.Panel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel6.Name = "Panel6";
+            this.Panel6.Size = new System.Drawing.Size(71, 302);
+            this.Panel6.TabIndex = 11;
+            // 
+            // Panel3
+            // 
+            this.Panel3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Panel3.Location = new System.Drawing.Point(512, 322);
+            this.Panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel3.Name = "Panel3";
+            this.Panel3.Size = new System.Drawing.Size(68, 241);
+            this.Panel3.TabIndex = 12;
+            // 
+            // Panel7
+            // 
+            this.Panel7.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Panel7.Controls.Add(this.label1);
+            this.Panel7.Location = new System.Drawing.Point(1660, 322);
+            this.Panel7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel7.Name = "Panel7";
+            this.Panel7.Size = new System.Drawing.Size(93, 79);
+            this.Panel7.TabIndex = 13;
+            this.Panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel7_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(17, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ziel";
+            this.label1.UseMnemonic = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1849, 757);
+            this.Controls.Add(this.Panel7);
+            this.Controls.Add(this.Panel3);
+            this.Controls.Add(this.Panel6);
+            this.Controls.Add(this.Panel5);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Panel12);
+            this.Controls.Add(this.Panel11);
+            this.Controls.Add(this.Panel4);
+            this.Controls.Add(this.Panel9);
+            this.Controls.Add(this.Panel10);
+            this.Controls.Add(this.Panel2);
+            this.Controls.Add(this.Panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormBall";
+            this.Text = "FormBall";
+            this.Panel7.ResumeLayout(false);
+            this.Panel7.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Panel1;
+        private System.Windows.Forms.Panel Panel2;
+        private System.Windows.Forms.Panel Panel10;
+        private System.Windows.Forms.Panel Panel4;
+        private System.Windows.Forms.Panel Panel11;
+        private System.Windows.Forms.Panel Panel9;
+        private System.Windows.Forms.Panel Panel12;
+        private System.Windows.Forms.Button Ball;
+        private System.Windows.Forms.Panel Panel5;
+        private System.Windows.Forms.Panel Panel6;
+        private System.Windows.Forms.Panel Panel3;
+        private System.Windows.Forms.Panel Panel7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsBall_4/WindowsFormsBall_2/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsBall_2
+{
+    public partial class FormBall : Form
+    //In der Form werden 12 Panel erzeugt in von Panel1 -12 benannt , erhalten grauen Hintergrund ( weitere Formatierung siehe Abbildung)
+    //ein Button wird erzeugt und in  Ball umbenannt und blauer Hintergrund (Eigenschaft)
+    //der Button erhält noch das Ereignis keydown mit der Bezeichnung BallKey
+
+    {
+
+       int bewegung=5;//Variable für die Bewegung bzw. Schrittgröße
+        int counter = 0;//Zähler für den Timer
+      
+ 
+
+
+        public FormBall()//Form1 wird erzeugt       
+        {
+            InitializeComponent();//Steuerelemente werden intialisiert
+            Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+            Ball.Left = 100;
+
+        }
+        
+      
+
+        private void BallKey(object sender, KeyEventArgs keyEventArgs)//
+
+        {
+            timer1.Start();//Timer wird gestartet
+         
+            //if (keyEventArgs.KeyCode ==(Keys.ShiftKey | Keys.W))//schneller oben
+
+            //  {
+            //      Ball.Top -= bewegung2;
+            //  }
+
+            if (keyEventArgs.KeyCode == Keys.W)//W-Taste
+
+            {
+                Ball.Top -= bewegung;
+
+            }
+
+            if (keyEventArgs.KeyCode == Keys.ShiftKey)//Beschleunigung
+
+            {
+                bewegung=30;
+            }
+
+            if (keyEventArgs.KeyCode == Keys.S)//unten
+
+            {
+                Ball.Top += bewegung;
+            }
+
+            if (keyEventArgs.KeyCode == Keys.A)//Space=Leertaste
+
+            {
+                Ball.Left -= bewegung;
+            }
+            if (keyEventArgs.KeyCode == Keys.D)//Space=Leertaste
+
+            {
+                Ball.Left += bewegung;
+            }
+
+            if ((keyEventArgs.KeyCode == Keys.W) && (keyEventArgs.KeyCode == Keys.D))//DIAGONAL NACH rechts OBEN
+
+            {
+            }
+
+            if ((keyEventArgs.KeyCode == Keys.S) && (keyEventArgs.KeyCode == Keys.D))//DIAGONAL NACH rechts unten
+
+            {
+                //keine Anweisungen nötig,da Anweisungen übernommen werden
+            }
+
+
+            if ((keyEventArgs.KeyCode == Keys.W) && (keyEventArgs.KeyCode == Keys.ShiftKey))//schneller NACH OBEN
+
+            {
+            }
+
+            if ((keyEventArgs.KeyCode == Keys.ShiftKey) && (keyEventArgs.KeyCode == Keys.D))//rechts schnell
+
+            {
+            }
+
+            //Kollision
+
+            if (Ball.Bounds.IntersectsWith(Panel1.Bounds))
+
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+
+            if (Ball.Bounds.IntersectsWith(Panel2.Bounds))
+
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+
+            if (Ball.Bounds.IntersectsWith(Panel3.Bounds))
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+
+            if (Ball.Bounds.IntersectsWith(Panel4.Bounds))
+
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+
+
+            if (Ball.Bounds.IntersectsWith(Panel5.Bounds))
+
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+
+            if (Ball.Bounds.IntersectsWith(Panel6.Bounds))
+
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+
+
+            if (Ball.Bounds.IntersectsWith(Panel7.Bounds))
+
+            {
+                timer1.Stop();
+                //Message Box-Aufbau: Erst Text inkl. Variable, dann Name der Box und dann Button, getrennt jeweisl durch Komma
+                MessageBox.Show("Ziel erreicht in "+ counter+" Sekunden", "Spielende",  MessageBoxButtons.OK);
+
+                Close();
+            }
+
+
+            if (Ball.Bounds.IntersectsWith(Panel9.Bounds))
+
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+            if (Ball.Bounds.IntersectsWith(Panel10.Bounds))
+
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+            if (Ball.Bounds.IntersectsWith(Panel11.Bounds))
+
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+            if (Ball.Bounds.IntersectsWith(Panel2.Bounds))
+
+            {
+                Ball.Top = 100;//Ausgangspostion des Balls, Nullpunkt links oben
+                Ball.Left = 100;
+            }
+
+        }
+        //Timer wird mit Toolbox unter der Form eingefügt und kann wie jedes Steuerelement bearbeitet werden
+        //Methode für den Timer, addiert immer bei jedem Tick , über den Ereignishändler das Intervall auf eine Sekunde (1000) gestellt.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+        }
+
+        private void Panel7_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+      
+    }
+
